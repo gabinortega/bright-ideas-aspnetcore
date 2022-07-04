4b31bf77a1ff1deb9e08bbf5de57c813e7ff1e3e
Gabino <gabinortega@gmail.com>
2022-07-03 20:13:29 -0400

    Change Swashbuckle.AspNetCore with NSwag.AspNetCore. Install NSwag.MSBuild to auto-generate typescript files (not configure yet)

--- src/BrightIdeas.Api/Program.cs
+++ src/BrightIdeas.Api/Program.cs
@@ -5,15 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerDocument(configure => configure.Title = "Brilliant Ideas");
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseOpenApi(); // serve documents (same as app.UseSwagger())
+    app.UseSwaggerUi3(); // serve Swagger UI
+    //app.UseReDoc(); // serve ReDoc UI
 }
 
 app.UseHttpsRedirection();
