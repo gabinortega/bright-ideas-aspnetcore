94acd3dabec2002df0f16e3ab7472fd39caf714a
Gabino <gabinortega@gmail.com>
2022-07-03 20:00:07 -0400

    Remove WeatherForecast Controller and functionality. Add Initial Ideas Controller

---
+++ src/BrightIdeas.Api/Controllers/IdeasController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BrightIdeas.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IdeasController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Hola !!!");
+        }
+    }
+}
